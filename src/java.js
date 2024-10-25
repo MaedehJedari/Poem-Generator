37c72f878278d6e30ee72c0ae6d86339045f0794
MaedehJedari <maedeh.jedari@gmail.com>
2024-10-25 09:22:26 +0330

    Added a poem

--- src/java.js
+++ src/java.js
@@ -13,14 +13,14 @@ function generatePoem(event) {
   let apiKey = "b5f67t0d61o0400c417eac73b698a670";
   let apiPrompt = `Generate a quatrain poem about ${userCommandElement.value}`;
   let apiContext =
-    "You are the best poet in the world who is an expert in writing quatrains. Write quatrains in basic HTML format and put a <br /> between each line and dont write any html or backticks";
+    "You are the best poet in the world who is an expert in writing quatrains. Write quatrains in basic HTML format and put a <br /> between each line and dont write any html or backticks. Sign the quatrain with `<br/><strong>SheCodes AI</strong>`";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
 
   //let poemElement = document.querySelector("#poem");
   // poemElement.style.display = "block";
 
   let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
+  //poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="blink">⏳ Generating a poem about ${userCommandElement.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
